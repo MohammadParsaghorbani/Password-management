18973d79327dff3a75b8f9a1216d4ec89dc99b29
MohammadParsaghorbani <mparsagh221@gmail.com>
2024-09-05 00:42:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ numbers = "0123456789"
 all = lower + uppper + symbols + numbers
 
 while True:
-    print("choose an option :\n\t1) create a password\n\t2) check all of your passwords\n\t3) exit")
+    print("choose an option :\n\t1) create a password\n\t2) check all of your passwords\n\t3) change password\n\t4) exit")
     choise = input("your choice : ")
     if choise == "1":
         site = input("enter the site you want to make password for it: ")
@@ -19,16 +19,35 @@ while True:
         length = int(input("enter the password length : "))
         password = "".join(random.sample(all, length))
         print(password)
-        file = open("password.txt" , "a")
+        file = open(f"{user_name}.txt" , "a")
         file.write(f"\n======================\nsite = {site}\nuser_name = {user_name}\ndescription = {description}\nlength of password = {length}\npassword = {password}")
         file.close()
     elif choise == "2":
-        file = open("password.txt" , "r")
+        file = open(f"{user_name}.txt" , "r")
         content = file.read()
         print(content)
         print("="*22)
         file.close()
     elif choise == "3":
+        file_path = input("enter your user name that you enter first: ")
+        with open(f"{file_path}.txt") as file:
+            print(file.read())
+        def change(file_path,line,text):
+            with open(f"{file_path}.txt") as file:
+                lines = file.readlines()
+            if (line <= len(lines)):
+
+                lines[line - 1] = text + "\n"
+
+                with open(f"{file_path}.txt" , "w") as file:
+                    for i in lines:
+                        file.write(i)
+            else:
+                print("not in file")
+        line = int(input("line: "))
+        text = input("text: ")
+        change(file_path,line,text)
+    elif choise == "4":
         print("good bye!")
         break
     else:
